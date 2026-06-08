6d28062e208c6ffae1fbe0f6d5fff8d04ae8d46d
angelieca25-max <angelieca25@itbss.ac.id>
2026-06-09 00:44:53 +0700

    update menu

---
+++ admin/hapus-menu.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+$conn = mysqli_connect("localhost", "root", "", "db_getcoffee");
+
+if (isset($_GET['id'])) {
+    $id_menu = $_GET['id'];
+
+    $sql = "DELETE FROM menu WHERE id_menu = '$id_menu'";
+    $query = mysqli_query($conn, $sql);
+
+    if ($query) {
+        echo "<script>
+                alert('Data menu berhasil dihapus!');
+                window.location.href = 'menu.php';
+              </script>";
+    } else {
+        echo "<script>
+                alert('Gagal menghapus data!');
+                window.location.href = 'menu.php';
+              </script>";
+    }
+} else {
+    header("Location: menu.php");
+    exit;
+}
+?>
